4474ea40ceebca1bdb3f4d8c8ed08b813566d448
jpt4 <jpt4@protonmail.ch>
2022-06-16 04:52:37 +0000

    sjas.org notes node

--- code/stellar/src/stellar/core.clj
+++ code/stellar/src/stellar/core.clj
@@ -7,6 +7,20 @@
              :refer :all :rename {== ==o}]
             :reload-all))
 
+(defn stellar-resolution [diagram out]
+  'stellar-resolution)
+
+(define fusion [star1 ray1 star2 ray2 out]
+  (fresh [su sol]
+    (star-union star1 star2 su)
+    (solution ray1 ray2 sol)
+    (apply-solution sol su out)))
+    
+
+;(defn signature [[vars func arity]]
+ ; (fresh [v]
+  ;  (==o vars v
+
 (defn foo
   "I don't do a whole lot."
   [x]
